aa5027fc3ca6d49c4cf1631412d082fd73331ba7
majaalin <maja_alin@hotmail.com>
2019-10-23 10:44:27 +0200

    Add array with authors

--- data.php
+++ data.php
@@ -1,3 +1,31 @@
 <?php
 
 declare(strict_types=1);
+
+$authors = [
+    [
+        'id' => '01',
+        'fullName' => 'Gunner Aguirre'
+
+    ],
+    [
+        'id' => '02',
+        'fullName' => 'Chaya Hardy'
+
+    ],
+    [
+        'id' => '03',
+        'fullName' => 'Tara Robertson'
+
+    ],
+    [
+        'id' => '04',
+        'fullName' => 'Reilly Riley'
+
+    ],
+    [
+        'id' => '05',
+        'fullName' => 'Blaine Branch'
+
+    ],
+];
