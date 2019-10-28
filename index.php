2bd7e5ce5bb2c6f2e92e44eb54302999a159b293
majaalin <maja_alin@hotmail.com>
2019-10-28 22:05:18 +0100

    Change css and pictures

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 require __DIR__ . '/data.php';
 require __DIR__ . '/functions.php';
 
+
 // Sort articles by date
 
 usort($articles, 'sortArticlesByDate');
@@ -16,32 +17,30 @@ usort($articles, 'sortArticlesByDate');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Source+Sans+Pro:400,600&display=swap" rel="stylesheet">    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Karla|Montserrat|Open+Sans:400,700|Roboto:400,700&display=swap" rel="stylesheet">
+    <title>Document</title>
 </head>
 <body>
     <header>
-        <img class="logo" src="logo.png" alt="logo">
-        <div class='search'>
-        <input type="text" placeholder="Search..">
-        <button><img src="search.png" alt="search"></button>
-        </div>
-        <img src="hamburger.png" alt="menu">
+        <h1>Conseqel</h1>
+        <img src="hamburger.png" alt="">
     </header>
     <main>
         <?php foreach ($articles as $article) : ?>
             <article>
+                <div class="content">
                 <h2><?php echo $article['title']; ?></h2>
                 <img src="<?php echo $article['image']; ?>"  loading="lazy"; alt="<?php echo $article['title']; ?>">
-                <p><?php echo $article['content']; ?></p>
                 <ul>
-                    <li class="author-name"><?php echo getAuthorsName($article['author_id'], $authors);?></li>
+                    <li class="author-name">By: <?php echo getAuthorsName($article['author_id'], $authors);?></li>
                     <li class="published-date"><?php echo $article['published_date']; ?></li>
-                    <li class="like">
-                        <div class="number"><?php echo $article['like_counter']; ?></div>
-                    </li>
+                </ul>
+                <p><?php echo $article['content']; ?></p>
+                </div>
+                <ul>
+                    <li class="like">&#x2764;  <?php echo $article['like_counter']; ?></li>
                 </ul>
             </article>
         <?php endforeach ?>
     </main>
 </body>
-</html>
